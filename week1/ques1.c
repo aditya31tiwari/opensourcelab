6d28efeb7366c4717389ef2a7b22ec7fc00b9e66
aditya31tiwari <31adityatiwari08@gmail.com>
2025-08-01 10:28:19 +0530

    Update and rename ques1 to ques1.c

---
+++ week1/ques1.c
@@ -0,0 +1,31 @@
+/*Create a C program that takes a 3 digit number from the user and
+reverses its digits.*/
+int main 
+#include <stdio.h>
+
+int main() {
+    int number, reversedNumber, hundreds, tens, units;
+    printf("Enter a three-digit number: ");
+    scanf("%d", &number);
+
+   
+    if (number < 100 || number > 999) {
+        printf("Please enter a valid three-digit number.\n");
+        return 1; // Exit the program
+    }
+
+  
+    hundreds = number / 100;       // Get the hundreds place
+    tens = (number / 10) % 10;     // Get the tens place
+    units = number % 10;           // Get the units place
+
+    
+    reversedNumber = (units * 100) + (tens * 10) + hundreds;
+
+    // Display the reversed number
+    printf("Reversed number: %d\n", reversedNumber);
+
+    return 0;
+}
+
+}
