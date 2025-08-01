42b45c725f0c5e2422cf7b201c46cee9c0bff737
aditya31tiwari <31adityatiwari08@gmail.com>
2025-08-01 11:45:07 +0530

    Update qus4.py

--- week2/qus4.py
+++ week2/qus4.py
@@ -1,1 +1,6 @@
-
+unp = input("Enter numbers separated by space: ")
+num_list = list(map(int, unp.split()))
+print("Even numbers in the list:")
+for n in num_list:
+    if n % 2 == 0:
+        print(n)
