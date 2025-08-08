dedfb5418cf69594767773f84620c9ef7ee640fe
Aditya Tiwari <31adityatiwari08@gmail.com>
2025-08-08 10:27:57 +0530

    Create ques6.py

---
+++ week2/ques6.py
@@ -0,0 +1,12 @@
+'''6. Write a python program to reverse a string.
+ '''# Sample list with duplicates
+# Input string
+original = "Hello, World!"
+
+# Reverse using slicing
+reversed_string = original[::-1]
+
+# Display the result
+print("Original string:", original)
+print("Reversed string:", reversed_string)
+
